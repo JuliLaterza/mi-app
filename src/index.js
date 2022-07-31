fc65a2dc97ee1dc070bda805aba69c97e6e58680
Julián Laterza <68128985+JuliLaterza@users.noreply.github.com>
2022-07-30 23:07:37 -0300

    cargando archivos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const component = React.createElement('h1',null,"Hi"); //Creación de un componente de React.
+
+const x = () => <p>Hola Mundo!</p> //Elemento funcional que retorna contenido JSX.
+
+root.render(x()); //Con este metodo renderizamos un elemento.
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
