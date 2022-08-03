07e13b6d81bd66c9d3836219577b184588d64822
Julián Laterza <68128985+JuliLaterza@users.noreply.github.com>
2022-08-03 02:00:05 -0300

    v3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import { Component } from 'react'
 
-class App extends Component {
+class Button extends Component {
+  constructor(props) {
+    super(props)
+    console.log('Constructor',props);
+  }
+
+  componentDidMount() {
+  console.log('ComponentDidMount');
+  }
+
   render() {
+    console.log('Ejecutando metodo del button')
+    return (
+      <button>
+        Enviar
+      </button>
+    )
+  } 
+}
+
+
+class App extends Component {
+  state = {
+    valor: 3
+  }
+  render() {
+    console.log('Ejecutando APP component');
     return (
       <div>
         <h3>Toca el botoncito por un helado</h3>
-        <button onClick={() => console.log('HELADO')}>Heladito 🍿🌭</button>
+        <Button chanchito = 'feliz'></Button>
+        <br></br>
+        <button className= {`${this.state.valor}`} onClick={() => this.setState({ valor: 2 })}>Heladito 🍿🌭</button>
       </div>
     )
   }
